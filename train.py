e1f5525ce69a1a6de7e6cdc463e0666dfd87adf7
mnee15 <rlaflswl0105@naver.com>
2022-12-08 01:38:33 +0000

    fix train.py/increment_path

--- train.py
+++ train.py
@@ -20,21 +20,21 @@ from utils.seed import seed_everything
 import wandb
 
 
-def increment_path(path, exist_ok=False):
-    """ Automatically increment path, i.e. runs/exp --> runs/exp0, runs/exp1 etc.
+def increment_path(model_dir, exp_name, exist_ok=False):
+    """ Automatically increment path, i.e. trained_models/exp --> trained_models/exp0, trained_models/exp1 etc.
     Args:
-        path (str or pathlib.Path): f"{model_dir}/{args.name}".
         exist_ok (bool): whether increment path (increment if False).
     """
+    path = osp.join(model_dir, exp_name)
     path = Path(path)
     if (path.exists() and exist_ok) or (not path.exists()):
-        return str(path)
+        return str(exp_name)
     else:
         dirs = glob.glob(f"{path}*")
         matches = [re.search(rf"%s(\d+)" % path.stem, d) for d in dirs]
         i = [int(m.groups()[0]) for m in matches if m]
         n = max(i) + 1 if i else 2
-        return f"{path}{n}"
+        return f"{exp_name}{n}"
 
 
 def parse_args():
