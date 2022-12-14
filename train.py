1329e95489d19ac4d5a414827252edba038953e6
uijin <abrahamkim98@gmail.com>
2022-12-14 08:10:10 +0000

    add valid

--- train.py
+++ train.py
@@ -3,6 +3,7 @@ import os
 import ast
 import argparse
 import os.path as osp
+import numpy as np
 import time
 import math
 from datetime import timedelta
@@ -20,6 +21,8 @@ from model import EAST
 
 from utils.seed import seed_everything
 import wandb
+from deteval import calc_deteval_metrics
+from detect import get_bboxes
 
 def arg_as_datadir_list(s):
     v=ast.literal_eval(s)
@@ -48,6 +51,7 @@ def parse_args():
     parser.add_argument('--input_size', type=int, default=512)
     parser.add_argument('--save_interval', type=int, default=1)
     parser.add_argument('--seed', type=int, default=214)
+    parser.add_argument('--val_interval', type=int, default=5)
 
     # 설정
     parser.add_argument('--learning_rate', type=float, default=5e-4)
@@ -55,9 +59,11 @@ def parse_args():
     parser.add_argument('--batch_size', type=int, default=2)
     parser.add_argument('--exp_name', type=str, default='test')
     parser.add_argument('--data_dir', type=arg_as_datadir_list, default="['ICDAR17_Korean']")
+    parser.add_argument('--val_data_dir', type=arg_as_datadir_list, default="['ICDAR17_Korean']")
     parser.add_argument('--total', type=int)
     parser.add_argument('--Weighted', type=arg_as_num_list, default="[1, 1, 1, 1, 1, 1]")
     parser.add_argument('--load_pre', type=str)
+
     
 
     args = parser.parse_args()
@@ -68,7 +74,7 @@ def parse_args():
     return args
 
 
-def do_training(data_dir, model_dir, device, image_size, input_size, num_workers, batch_size,
+def do_training(data_dir, model_dir, val_data_dir, val_interval, device, image_size, input_size, num_workers, batch_size,
                 learning_rate, max_epoch, save_interval, seed, exp_name, Weighted, total, load_pre=None):
     # fix seed
     seed_everything(seed)
@@ -111,8 +117,10 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
     scheduler = lr_scheduler.MultiStepLR(optimizer, milestones=[max_epoch // 2], gamma=0.1)
 
     model.train()
+    max_hmean = 0
     for epoch in range(max_epoch):
         epoch_loss, epoch_start = 0, time.time()
+        
         with tqdm(total=num_batches) as pbar:
             for img, gt_score_map, gt_geo_map, roi_mask in train_loader:
                 pbar.set_description('[Epoch {}]'.format(epoch + 1))
@@ -150,6 +158,79 @@ def do_training(data_dir, model_dir, device, image_size, input_size, num_workers
 
             ckpt_fpath = osp.join(model_dir, 'latest.pth')
             torch.save(model.state_dict(), ckpt_fpath)
+
+        if epoch % val_interval != 0:
+            continue
+        #########################################  validation  ############################################################################################################
+        val_dataset = SceneTextDataset(val_data_dir, split='train', image_size=image_size, crop_size=input_size)
+        val_dataset = EASTDataset(val_dataset)
+        val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=True, num_workers=num_workers)
+        gt_bboxes, pred_bboxes, transcriptions = [], [], []
+        with torch.no_grad():
+            print("Calculating validation results...")
+            model.eval()
+            
+            for img, gt_score_map, gt_geo_map, _ in val_loader:
+                orig_sizes = []
+                pred_bbox, gt_bbox, transcription = [], [], []
+
+                for image in img:
+                    orig_sizes.append(image.shape[:2]) 
+
+                pred_score_map, pred_geo_map = model.forward(img.to(device))
+                pred_score_map, pred_geo_map = pred_score_map.cpu().numpy(), pred_geo_map.cpu().numpy()
+                gt_score_map, gt_geo_map = gt_score_map.cpu().numpy(), gt_geo_map.cpu().numpy()
+
+                for pred_score, pred_geo, gt_score, gt_geo, orig_size in zip(pred_score_map, pred_geo_map, gt_score_map, gt_geo_map, orig_sizes):
+                    pred_bx = get_bboxes(pred_score, pred_geo)
+                    gt_bx = get_bboxes(gt_score, gt_geo)
+
+                    if pred_bx is None:
+                        pred_bx = np.zeros((0, 4, 2), dtype=np.float32)
+                    else:
+                        pred_bx = pred_bx[:, :8].reshape(-1, 4, 2)
+                        pred_bx *= max(orig_size) / input_size
+
+                    if gt_bx is None:
+                        gt_bx = np.zeros((0, 4, 2), dtype=np.float32)
+                        trans = []
+                    else:
+                        gt_bx = gt_bx[:, :8].reshape(-1, 4, 2)
+                        gt_bx *= max(orig_size) / input_size
+                        trans = ['null' for _ in range(gt_bx.shape[0])]
+
+                    pred_bbox.append(pred_bx)
+                    gt_bbox.append(gt_bx)
+                    transcription.append(trans)
+
+                pred_bboxes.extend(pred_bbox)
+                gt_bboxes.extend(gt_bbox)
+                transcriptions.extend(transcription)
+            
+            pred_bboxes_dict, gt_bboxes_dict, transcriptions_dict = dict(), dict(), dict()
+            img_len = len(val_dataset)
+            for img_num in range(img_len):
+                pred_bboxes_dict[f'{img_num}'] = pred_bboxes[img_num]
+                gt_bboxes_dict[f'{img_num}'] = gt_bboxes[img_num]
+                transcriptions_dict[f'{img_num}'] = transcriptions[img_num]
+            
+            deteval_metrics = calc_deteval_metrics(pred_bboxes_dict, gt_bboxes_dict, transcriptions_dict)['total']
+            # fix
+            print(f"[Val] Precision: {deteval_metrics['precision']:.4f} | Recall: {deteval_metrics['recall']:.4f} | Hamsumean: {deteval_metrics['hmean']:.4f}")
+
+            if deteval_metrics['hmean'] > max_hmean:
+                max_hmean = deteval_metrics['hmean']
+                if not osp.exists(model_dir):
+                    os.makedirs(model_dir)
+
+                print('best model changed!')
+                ckpt_fpath = osp.join(model_dir, 'best.pth')
+                torch.save(model.state_dict(), ckpt_fpath)
+            # wandb logging
+            wandb.log({
+                'Precision': deteval_metrics['precision'], 'Recall': deteval_metrics['recall'], 'Hansumean': deteval_metrics['hmean']
+            })
+        ###################################################################################################################################################################
 
     wandb.finish()
 
--- train.sh
+++ train.sh
@@ -1,12 +1,13 @@
 python train.py \
---exp_name '19ArT_Test' \
---load_pre /opt/ml/input/data/_ArchivePth/Pre06296_ICDAR15171919ArT_weight1312_total2500_.pth \
+--exp_name 'Up_Valid_15171919ArT_train' \
+--load_pre /opt/ml/input/data/_ArchivePth/Pre06296_ICDAR15171919ArT_weight1312_total2500_100Epoch.pth \
 --data_dir "['ICDAR15_Korean', 'ICDAR17_Korean', 'ICDAR19_ArT', 'ICDAR19_Korean']" \
+--val_data_dir "['Upstage']" \
 --total 2500 \
---Weighted "[1, 3, 1, 2]" \
---max_epoch 100 \
---batch_size 16
-
+--Weighted "[0.2, 0.2, 0.3, 0.3]" \
+--max_epoch 150 \
+--batch_size 16 \
+--learning_rate 5e-4
 # 실행방법
 # 1. sh train.sh
 # 2. ./train.sh
